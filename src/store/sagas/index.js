a433034f7c4a8e6e3c6640ca2d75aaeb8d350b6f
Valentyn Tishchenko <valentyntishchenko@gmail.com>
2019-02-23 20:59:13 +0200

    fixed

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,11 +1,9 @@
 import { all } from 'redux-saga/effects';
 import { watchForSignIn } from 'src/pages/SignIn/store/sagas';
-import { watchForSignUp } from 'src/pages/SignUp/store/sagas';
 
 export default function* () {
   yield all([
       watchForSignIn(),
-      watchForSignUp(),
     ]
   );
 }
